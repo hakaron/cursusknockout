74ca58b35346a94d81af18899666678e25ac3a4d
Koen <callewaert.koen@gmail.com>
2014-09-04 16:22:04 +0200

    bmi opdrachtje toegevoegd

---
+++ bmi.js
@@ -0,0 +1,32 @@
+﻿var viewModel = {
+    lengte: ko.observable(1.70),
+    gewicht: ko.observable(90),
+    bmi: function () {
+        var w = this.gewicht();
+        var l = this.lengte();
+        return w / (l * l);
+    },
+    boodschap: function () {
+        var mi = this.bmi();
+        if (mi < 18.5) {
+            return 'ondergewicht';
+        } else if (mi < 25) {
+            return 'normaal gewicht';
+        } else if (mi < 27) {
+            return 'licht overgewicht';
+        } else if (mi < 30) {
+            return 'matig overgewicht';
+        } else if (mi < 40) {
+            return 'ernstig overgewicht';
+        } else {
+            return 'fel overgewicht';
+        }
+    },
+    lichter: function () {
+        this.gewicht(this.gewicht() - 1);
+    },
+    zwaarder: function () {
+        this.gewicht(this.gewicht() + 1);
+    }
+};
+ko.applyBindings(viewModel);
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-﻿var viewModel = { voornaam: "Jean", familienaam: "Smits", wedde: 2500,
+﻿var viewModel = { voornaam: "Jean", familienaam: "Smits", wedde: ko.observable(2500),
     nettoWedde: function () {
-        alert(this.wedde / 2);
+        alert(this.wedde() / 2);
     },
     auto: function () {
-        return this.wedde < 7000 ? "golf" : "audi";
+        return this.wedde() < 7000 ? "golf" : "audi";
+    },
+    opslag: function () {
+        this.wedde(this.wedde() + 1000);
     }
 };
 ko.applyBindings(viewModel);
