66cc537751c64b9519f7f674d98837d09a342336
Koen <callewaert.koen@gmail.com>
2014-09-04 14:44:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿var viewModel = { voornaam: "Jean", familienaam: "Smits", wedde: 2500,
+    nettoWedde: function () {
+        alert(this.wedde / 2);
+    },
+    auto: function () {
+        return this.wedde < 7000 ? "golf" : "audi";
+    }
+};
+ko.applyBindings(viewModel);
