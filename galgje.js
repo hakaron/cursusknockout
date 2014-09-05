94473464dd923c8a98e4dbc88955ffb5ea5ef7be
Koen <callewaert.koen@gmail.com>
2014-09-05 11:04:00 +0200

    galgje.js: ViewModel uitgekuist (SoC)

--- galgje.js
+++ galgje.js
@@ -1,59 +1,56 @@
-﻿var viewModel = {
-    geradenLetters: [],
-    pogingen: ko.observable(0),
-    teRaden: '',
-    weergave: ko.observable(''),
-    gameOver: false,
-    kiesWoord: function () {
-        var woorden = ['cocktail', 'mayonaise', 'mosterd', 'tartare', 'vinaigrette'];
-        var index = Math.floor(Math.random() * woorden.length);
-        this.teRaden = woorden[index];
-        this.nieuweWeergave();
-    },
-    nieuweWeergave: function () {
-        var woord = this.teRaden;
-        var weerTeGeven = '';
-        for (var i = 0; i < woord.length; i++) {
-            var zitErin = false;
-            for (var j = 0; j < this.geradenLetters.length; j++) {
-                if (!zitErin && this.geradenLetters[j] == woord[i]) {
-                    weerTeGeven += woord[i];
-                    zitErin = true;
-                }
-            }
-            if (!zitErin) {
-                weerTeGeven += '.';
+﻿var geradenLetters = [];
+var gameOver = false;
+function kiesWoord() {
+    var woorden = ['cocktail', 'mayonaise', 'mosterd', 'tartare', 'vinaigrette'];
+    var index = Math.floor(Math.random() * woorden.length);
+    return woorden[index];
+}
+var woord = kiesWoord();
+function nieuweWeergave () {
+    var weerTeGeven = '';
+    for (var i = 0; i < woord.length; i++) {
+        var zitErin = false;
+        for (var j = 0; j < geradenLetters.length; j++) {
+            if (!zitErin && geradenLetters[j] == woord[i]) {
+                weerTeGeven += woord[i];
+                zitErin = true;
             }
         }
-        this.weergave(weerTeGeven);
-    },
+        if (!zitErin) {
+            weerTeGeven += '.';
+        }
+    }
+    return weerTeGeven;
+}
+var viewModel = {
+    pogingen: ko.observable(0),
+    weergave: ko.observable(nieuweWeergave()),
+    gameOver: false,
     raden: function () {
-        if (!this.gameOver) {
+        if (!gameOver) {
             var ingave = document.getElementById('letter').value;
             if (ingave.length == 1) {
                 var letter = ingave;
-                var woord = this.teRaden;
                 var zitErin = false;
                 for (var i = 0; i < woord.length; i++) {
                     if (!zitErin && woord[i] == letter) {
                         zitErin = true;
-                        this.geradenLetters.push(letter);
+                        geradenLetters.push(letter);
                     }
                 }
                 if (!zitErin) {
                     this.pogingen(this.pogingen() + 1);
                 }
-                this.nieuweWeergave();
+                this.weergave(nieuweWeergave());
             }
-            if (this.weergave() == this.teRaden) {
-                alert('U heeft gewonnen, de saus was ' + this.teRaden);
-                this.gameOver = true;
+            if (this.weergave() == woord) {
+                alert('U heeft gewonnen, de saus was ' + woord);
+                gameOver = true;
             } else if (this.pogingen() == 10) {
-                alert('U heeft verloren, de saus was ' + this.teRaden);
-                this.gameOver = true;
+                alert('U heeft verloren, de saus was ' + woord);
+                gameOver = true;
             }
         }
     }
 };
 ko.applyBindings(viewModel);
-viewModel.kiesWoord();
