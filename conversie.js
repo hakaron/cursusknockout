7a6c771af1153bcb6a5593f49a0c5c80334d61e2
Koen <callewaert.koen@gmail.com>
2014-09-05 15:50:55 +0200

    conversie opdrachtje toegevoegd

---
+++ conversie.js
@@ -0,0 +1,13 @@
+﻿var viewModel = {
+    afstand: ko.observable(2.54),
+    soort: ko.observable('CI'),
+    converteren: function () {
+        var test = this.afstand();
+        if (this.soort() == 'CI') {
+            return this.afstand() / 2.54;
+        } else if (this.soort() == 'IC') {
+            return this.afstand() * 2.54;
+        }
+    }
+};
+ko.applyBindings(viewModel);
--- index.js
+++ index.js
@@ -4,7 +4,14 @@
     wedde: ko.observable(2500),
     gehuwd: ko.observable(true),
     rijbewijs: ko.observable(),
+    provincie: ko.observable(),
+    geslacht: ko.observable("M"),
     rijbewijzen: ['A3', 'A', 'B', 'B + E', 'C', 'C + E', 'D1', 'D1 + E', 'D', 'D + E'],
+    provincies:[{naam: 'West-Vlaanderen', hoofdstad: 'Brugge'},
+                {naam: 'Oost-Vlaanderen', hoofdstad: 'Gent'},
+                {naam: 'Antwerpen', hoofdstad: 'Antwerpen'},
+                {naam: 'Limburg', hoofdstad: 'Hasselt'},
+                {naam: 'Vlaams-Brabant', hoofdstad: 'Leuven'}],
     nettoWedde: function () {
         alert(this.wedde() / 2);
     },
