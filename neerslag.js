130bbec5e9b447ee4069ff7f0a3c43f050702213
Koen <callewaert.koen@gmail.com>
2014-09-10 10:10:01 +0200

    toevoegen voorbeeld neerslag

---
+++ neerslag.js
@@ -0,0 +1,24 @@
+﻿var viewModel = {
+    dagen : [ {naam:"maandag", neerslag:ko.observable()},
+              {naam:"dinsdag", neerslag:ko.observable()},
+              {naam:"woensdag", neerslag:ko.observable()},
+              {naam:"donderdag", neerslag:ko.observable()},
+              {naam:"vrijdag", neerslag:ko.observable()},
+              {naam:"zaterdag", neerslag:ko.observable()},
+              {naam:"zondag", neerslag:ko.observable()}
+            ],
+    gemiddelde: function () {
+        var ingevuldeTemperaturen = this.dagen.filter(
+            function(element) {return !isNaN(element.neerslag());});
+        var aantalIngevuldeTemperaturen = ingevuldeTemperaturen.length;
+        if (aantalIngevuldeTemperaturen === 0) {
+            return "";
+        }
+        var somTemperaturen = ingevuldeTemperaturen.reduce(
+            function(previousValue, element){
+                return previousValue + parseInt(element.neerslag());}, 0);
+        return somTemperaturen / aantalIngevuldeTemperaturen;
+    }
+};
+ko.applyBindings(viewModel);
+document.querySelector("tbody tr:first-child td:nth-child(2) input").focus();
