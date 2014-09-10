cb1e25e42d39858f24e5fd74036aa083e2458e06
Koen <callewaert.koen@gmail.com>
2014-09-10 14:32:23 +0200

    opdracht frituur toegevoegd

---
+++ frituur.js
@@ -0,0 +1,32 @@
+﻿var viewModel = {
+    bestelling: ko.observableArray(),
+    producten: [{ naam: "Kleine friet", prijs: 1.75 },
+                { naam: "Medium friet", prijs: 2.15 },
+                { naam: "Grote friet", prijs: 2.5 },
+                { naam: "Mayonaise", prijs: 0.65 },
+                { naam: "Kippenboutjes", prijs: 3.25 },
+                { naam: "Stoofvlees", prijs: 4.35 }
+               ],
+    toevoegen: function () {
+        this.bestelling.push({
+            product: ko.observable({
+                naam: "Maak een keuze",
+                prijs: 0
+            }),
+            aantal: ko.observable(1)
+        });
+    },
+    totaal: function () {
+        var totaal = 0;
+        var besteld = this.bestelling();
+        for (var i = 0; i < besteld.length; i++) {
+            var product = besteld[i].product();
+            var aantal = besteld[i].aantal();
+            if (product != undefined && !isNaN(product.prijs) && aantal != undefined && !isNaN(aantal)) {
+                totaal += product.prijs * aantal;
+            }
+        }
+        return totaal;
+    }
+};
+ko.applyBindings(viewModel);
