4be643789d8982fd492c7174b728604c8938d7f9
Koen <callewaert.koen@gmail.com>
2014-09-10 09:18:03 +0200

    afwerken fruit opdracht

--- fruit.js
+++ fruit.js
@@ -1,1 +1,11 @@
-﻿
+﻿var viewModel = {
+    fruitsoorten: [{ naam: "Aalbessen", calorie: 38 },
+                   { naam: "Aardbeien", calorie: 36 },
+                   { naam: "Abrikozen", calorie: 60 },
+                   { naam: "Ananas", calorie: 58 }],
+    gekozenFruit: ko.observable(),
+    detail: function (fruit) {
+        viewModel.gekozenFruit(fruit);
+    }
+};
+ko.applyBindings(viewModel);
