21b363b0d5075089939afc344477a7815b1f88d4
Koen <callewaert.koen@gmail.com>
2014-09-10 12:25:46 +0200

    Todo voorbeeld uitgewerkt

---
+++ todo.js
@@ -0,0 +1,13 @@
+﻿var viewModel = {todos : ko.observableArray(),
+    toevoegen: function() {
+        this.todos.push({ omschrijving: ko.observable(""), afgewerkt: ko.observable(false) });
+        document.querySelector("tbody tr:last-child td:first-child input").focus();
+    },
+    verwijderen: function(teVerwijderenElement) {
+        viewModel.todos.remove(teVerwijderenElement);
+    },
+    afgewerkteVerwijderen: function() {
+        viewModel.todos.remove(function(element){return element.afgewerkt();});
+    }
+};
+ko.applyBindings(viewModel);
