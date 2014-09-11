3fa3339271ccf6dbeebd875ba936db8a46f1fe61
koen <koen@hakaron.eu>
2014-09-11 19:03:28 +0200

    bijwerken verjaardagen.js en nu verwijst de html naar het correcte script

--- verjaardagen.js
+++ verjaardagen.js
@@ -1,4 +1,5 @@
-﻿var dagrij = [];
+﻿//arrays voor dagen en maanden aanmaken
+var dagrij = [];
 var maandrij = [];
 for (var i = 0; i < 31; i++) {
     dagrij.push(i + 1);
@@ -6,13 +7,14 @@ for (var i = 0; i < 31; i++) {
 for (var i = 0; i < 12; i++) {
     maandrij.push(i + 1);
 }
+//viewModel
 var viewModel = {
     verjaardagen: ko.observableArray(),
     dagen: dagrij,
     maanden: maandrij,
     toevoegen: function () {
         var verjaardag = {
-            naam: ko.observable(), dag: ko.observable(1), maand: ko.observable()
+            naam: ko.observable(), dag: ko.observable(1), maand: ko.observable(1)
         };
         this.verjaardagen.push(verjaardag);
         document.querySelector("tbody tr:last-child td:first-child input").focus();
@@ -24,3 +26,26 @@ var viewModel = {
     }
 };
 ko.applyBindings(viewModel);
+//storage opties
+function opslaan() {
+    window.localStorage.setItem("verjaardagen", ko.toJSON(viewModel.verjaardagen));
+}
+function laden() {
+    var verjaardagen = window.localStorage.getItem("verjaardagen");
+    var verjaardagMetObservables;
+    if (verjaardagen !== null) {
+        verjaardagen = JSON.parse(verjaardagen);
+        verjaardagen.forEach(function (verjaardag) {
+            verjaardagMetObservables = {
+                naam: ko.observable(verjaardag.naam),
+                dag: ko.observable(verjaardag.dag),
+                maand: ko.observable(verjaardag.maand)
+            };
+            viewModel.verjaardagen.push(verjaardagMetObservables);
+            verjaardagMetObservables.naam.subscribe(function () { opslaan(); });
+            verjaardagMetObservables.dag.subscribe(function () { opslaan() });
+            verjaardagMetObservables.maand.subscribe(function () { opslaan() });
+        });
+    }
+}
+laden();
