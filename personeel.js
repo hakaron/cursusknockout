71f343784a999fa2b6b7c23ce7e40fb7b2c0e3bb
Koen <callewaert.koen@gmail.com>
2014-09-12 15:28:30 +0200

    Uitbreiding opdracht personeel

--- personeel.js
+++ personeel.js
@@ -1,20 +1,42 @@
 ﻿var viewModel = {
+    personeel: ko.observableArray(),
     gekozen: ko.observable(),
-    template: ko.observable("personeel"),
-    toonPersoneel: function() {
-        viewModel.template("personeel");
-    },
-    detail: function(keuze) {
-        viewModel.gekozen(keuze);
-        viewModel.template("detail");
-    }
+    template: ko.observable()
 };
-var request = new XMLHttpRequest();
-request.open("GET", "personeel.json", true);
-request.onload = function () {
-    if (this.status === 200) {
-        viewModel.personeel = JSON.parse(this.responseText);
-        ko.applyBindings(viewModel);
+Sammy(function () {
+    this.get("#personeel/:nummer", function () {
+        var nummer = parseInt(this.params["nummer"]);
+        vulPersoneel();
+        vulDetail(nummer);
+        viewModel.template("detail")
+    });
+    this.get("", function () {
+        vulPersoneel();
+        viewModel.template("personeel");
+    });
+}).run();
+function vulPersoneel() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "personeel.json", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            viewModel.personeel(JSON.parse(request.responseText));
+        }
     }
+    request.send();
 }
-request.send();
+function vulDetail(nummer) {
+    var personeelslid = null;
+    var personeel = viewModel.personeel();
+    for (var i = 0; i < personeel.length; i++) {
+        if (personeel[i].nummer === nummer) {
+            personeelslid = personeel[i];
+        }
+    }
+    if (personeelslid === null) {
+        personeelslid = { voornaam: "Ongeldig", familienaam: "personeelslid!", nummer: "N/A",
+            wedde: "N/A", job: "N/A" };
+    }
+    viewModel.gekozen(personeelslid);
+}
+ko.applyBindings(viewModel);
