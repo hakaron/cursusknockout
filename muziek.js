daa926df9f8a8f4bf6623aa132319e7628ea8bf0
Koen <callewaert.koen@gmail.com>
2014-09-12 14:23:42 +0200

    Aanpassen voorbeeld muziek

--- muziek.js
+++ muziek.js
@@ -2,23 +2,40 @@
     genre: ko.observable(),
     artiesten: ko.observableArray()
 };
-var request = new XMLHttpRequest();
-request.open("GET", "genres.json", true);
-request.onload = function () {
-    if (this.status === 200) {
-        viewModel.genres = JSON.parse(this.responseText);
-        ko.applyBindings(viewModel);
+Sammy(function () {
+    this.get("#genres/:nummer", function () {
+        var genreNummer = parseInt(this.params["nummer"]);
+        if (viewModel.genres === undefined) {
+            vulGenres();
+        }
+        vulArtiesten(genreNummer);
+    });
+    this.get("", function () {
+        if (viewModel.genres === undefined) {
+            vulGenres();
+        }
+        viewModel.artiesten.removeAll();
+    });
+}).run();
+function vulGenres() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "genres.json", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            viewModel.genres = JSON.parse(request.responseText);
+            ko.applyBindings(viewModel);
+        }
     }
+    request.send();
 }
-request.send();
-viewModel.kiesGenre = function (gekozenGenre) {
-    viewModel.genre(gekozenGenre);
-    request.open("GET", gekozenGenre.link.href, true);
-    request.onload = function () {
-        if (this.status === 200) {
-            var artiesten = JSON.parse(this.responseText);
+function vulArtiesten(genreNummer) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "genres." + genreNummer + ".artists.json", true);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var artiesten = JSON.parse(request.responseText);
             viewModel.artiesten(artiesten);
         }
-    };
+    }
     request.send();
 }
