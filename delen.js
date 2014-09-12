7d53efeb1ea8e7a64edfeacad2fea18ea79a1241
Koen <callewaert.koen@gmail.com>
2014-09-12 16:30:53 +0200

    einde les, deel 1 validation

---
+++ delen.js
@@ -0,0 +1,15 @@
+﻿var viewModel = {
+    teller: ko.observable(),
+    noemer: ko.observable(),
+    deling: function() {
+        if (this.isValid()) {
+            return this.teller() / this.noemer();
+        } else {
+            return "";
+        }
+    }
+}
+ko.validation.init({
+    parseInputAttributes: true
+});
+ko.applyBindings(ko.validatedObservable(viewModel));
