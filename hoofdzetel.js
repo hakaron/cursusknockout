f5aca74d421ede5875d2663248352b1e3007015c
Koen <callewaert.koen@gmail.com>
2014-09-12 12:00:57 +0200

    voorbeeld personeel toegevoed, werkt ondanks slecht design

---
+++ hoofdzetel.js
@@ -0,0 +1,11 @@
+﻿var viewModel = {
+    straat: "Keizerslaan", huisnummer: 10, postcode: 1000, gemeente: "Brussel",
+    template: ko.observable("adres"),
+    adres: function() {
+        this.template("adres");
+    },
+    foto: function() {
+        this.template("foto");
+    }
+};
+ko.applyBindings(viewModel);
---
+++ personeel.js
@@ -0,0 +1,21 @@
+﻿var viewModel = {
+    gekozen: ko.observable(),
+    template: ko.observable("personeel"),
+    toonPersoneel: function() {
+        this.template("personeel");
+    },
+    detail: function(keuze) {
+        viewModel.gekozen(keuze);
+        viewModel.template("detail");
+    }
+};
+var request = new XMLHttpRequest();
+request.open("GET", "personeel.json", true);
+request.onload = function () {
+    if (this.status === 200) {
+        viewModel.personeel = JSON.parse(this.responseText);
+        viewModel.gekozen(viewModel.personeel[0])
+        ko.applyBindings(viewModel);
+    }
+}
+request.send();
